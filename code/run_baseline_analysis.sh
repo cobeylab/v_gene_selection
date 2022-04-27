7d00ae4d24905ec7cb41ffe934c8afc383955a29
Marcos Costa Vieira <mvieira@midway2-login2.rcc.local>
2022-04-27 16:45:41 -0500

    Updated some sbatch parameters to run some scripts on Midway3

--- code/run_baseline_analysis.sh
+++ code/run_baseline_analysis.sh
@@ -17,7 +17,7 @@ do
     echo "#SBATCH --account=pi-cobey" >> $sbatch_file
     echo "#SBATCH -o out_err_files/run_baseline_$mid.out" >> $sbatch_file       
     echo "#SBATCH -e out_err_files/run_baseline_$mid.err" >> $sbatch_file         
-    echo "#SBATCH --time=100:00:00" >> $sbatch_file
+    echo "#SBATCH --time=20:00:00" >> $sbatch_file
     echo "#SBATCH --mem-per-cpu=4000" >> $sbatch_file
 
     echo module load R/3.6.1 >> $sbatch_file
