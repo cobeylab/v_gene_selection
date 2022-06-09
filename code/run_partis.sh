1345c0a0c9f9549fe115e0b6d3fb9354e6981b1a
Marcos Costa Vieira <mvieira@midway2-login2.rcc.local>
2022-06-09 12:06:40 -0500

    Added newline to partis output to prevent warning; code for automatically creating results dirs

--- code/run_partis.sh
+++ code/run_partis.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 time_point=$1
 
+# Create partis results directory (if it doesn't exist)
+mkdir -p ../results/partis/
+
+# Directory for keeping cluster out and err files
+mkdir -p out_err_files
+
 sbatch_file=run_partis_day_${time_point}.sbatch
 # --------------- SBATCH FILE ---------
 echo '#!/bin/bash' > $sbatch_file
@@ -13,6 +19,7 @@ echo "#SBATCH --nodes=1" >> $sbatch_file
 echo "#SBATCH --ntasks-per-node=16" >> $sbatch_file
 echo "#SBATCH --mem-per-cpu=3000" >> $sbatch_file
     
+#echo module load mafft >> $sbatch_file
 echo input_file=../processed_data/mouse_specific_data_files/$time_point-'${SLURM_ARRAY_TASK_ID}'.csv >> $sbatch_file
 
 
@@ -30,19 +37,23 @@ echo else >> $sbatch_file
     echo temp_fasta_file=../processed_data/mouse_specific_data_files/$time_point-'${SLURM_ARRAY_TASK_ID}'.fasta >> $sbatch_file
 
     # Run partis
-    echo /project2/cobey/partis/bin/partis partition --n-procs 16 --species mouse --infname '$temp_fasta_file' --outfname '$output_file' --extra-annotation-columns regional_bounds:cdr3_seqs:seqs_aa:naive_seq_aa:consensus_seq:consensus_seq_aa >> $sbatch_file
+    echo /project2/cobey/partis/bin/partis partition --n-procs 15 --species mouse --infname '$temp_fasta_file' --outfname '$output_file' --extra-annotation-columns regional_bounds:cdr3_seqs:seqs_aa:naive_seq_aa:consensus_seq:consensus_seq_aa >> $sbatch_file
 
     # Remove temporary fasta file
     echo rm '$temp_fasta_file' >> $sbatch_file
+    
+    # Add new line to partis output to avoid R's read_yaml complaining
+    echo 'echo "" >> $output_file' >> $sbatch_file
 
 echo fi >> $sbatch_file
+
 
 
 #echo "rm -r _output" >> $sbatch_file
 # ---------------------------------------
 
 # Find all mouse numbers for the specified time point
-ids=$(ls ../data/sequence_data/mouse_specific_data_files/$time_point-*.csv | grep -o "\-[0-9]*" | tr '\n' ',' | tr -d '-')
+ids=$(ls ../processed_data/mouse_specific_data_files/$time_point-*.csv | grep -o "\-[0-9]*" | tr '\n' ',' | tr -d '-')
 ids=${ids::-1}
 
 
